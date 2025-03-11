9c8b88a7b31d847f51fb1d0a856fd00394b58a21
Nirawit Jongprasartsuk <nirawitrordor@gmail.com>
2025-03-11 21:45:16 +0700

    Creating home menu & setting menu [Structure-wise]

---
+++ src/menus/setting_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_audio_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_music_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_logout_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/menus/home_menu.tscn")
