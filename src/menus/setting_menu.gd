15dd9ffecbddad75d883fa23a081febbdff003c8
DEC-48 <tanwajava@gmail.com>
2025-03-12 16:05:58 +0700

    Add music background and slider

--- src/menus/setting_menu.gd
+++ src/menus/setting_menu.gd
@@ -1,10 +1,25 @@
 extends Control
-
-
-
-func _on_audio_pressed() -> void:
-	pass # Replace with function body.
+@onready var backgroundMusicSlider: Slider = %bgMusic_slider;
+@onready var sfxSlider: Slider = %sfx_slider;
+func _ready():
+	backgroundMusicSlider.value = db_to_linear(
+		AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BackgroundMusic"))
+	)
+	sfxSlider.value = db_to_linear(
+		AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Sfx"))
+	)
 	
+func _on_BackgroundMusicSlider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BackgroundMusic"), linear_to_db(value))
+	print(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BackgroundMusic")))
+
+func _on_sfxSlider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sfx"), linear_to_db(value))
+	print(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("")))
+	
+	
+
+
 
 func _on_music_pressed() -> void:
 	pass # Replace with function body.
