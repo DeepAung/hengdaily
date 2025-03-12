19e02d87a01424b3060a26e4bd3ee048f745860e
Nirawit Jongprasartsuk <nirawitrordor@gmail.com>
2025-03-12 22:15:21 +0700

    Change bgm, add hover and click sound (bus : sfx)

--- src/menus/setting_menu.gd
+++ src/menus/setting_menu.gd
@@ -11,23 +11,30 @@ func _ready():
 	
 func _on_BackgroundMusicSlider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BackgroundMusic"), linear_to_db(value))
-	print(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BackgroundMusic")))
+	#print(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BackgroundMusic")))
 
 func _on_sfxSlider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sfx"), linear_to_db(value))
-	print(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("")))
-	
-	
-
-
-
-func _on_music_pressed() -> void:
-	pass # Replace with function body.
+	#print(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("")))
 
 
 func _on_logout_pressed() -> void:
-	pass # Replace with function body.
+	$Click.play()
+	
+
+func _on_logout_mouse_entered() -> void:
+	$"Hover-card".play()
 
 
 func _on_back_pressed() -> void:
+	$Click.play()
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 0.01
+	timer.one_shot = true
+	timer.start()
+	await timer.timeout
 	get_tree().change_scene_to_file("res://src/menus/home_menu.tscn")
+
+func _on_back_mouse_entered() -> void:
+	$"Hover-card".play()
